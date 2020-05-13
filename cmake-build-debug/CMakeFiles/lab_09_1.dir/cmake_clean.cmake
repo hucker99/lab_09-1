c4dcf7fb01c1729e162c832c56da15891316a19b
hucker99 <vereshagin.dmitry99@yandex.ru>
2020-05-13 17:20:41 +0300

    Initial

---
+++ cmake-build-debug/CMakeFiles/lab_09_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_09_1.dir/main.cpp.o"
+  "lab_09_1"
+  "lab_09_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_09_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
